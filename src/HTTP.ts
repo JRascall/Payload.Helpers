4f19d70c9de7bfe0d0f08f620904830f85860eeb
Jamie Mascall <jamie.mascall@parkersoftware.com>
2022-09-18 20:19:41 +0100

    Added HTTP class

---
+++ src/HTTP.ts
@@ -0,0 +1,168 @@
+﻿import Hooks from "./Hook";
+
+export default class HTTP {
+    constructor(
+        protected readonly hooks: Hooks
+    ) { }
+
+    /**
+     * make a get request to an http url and return the result
+     * @param {string} address url to get
+     * @param {(response: unknown) => unknown}} cb callback at end
+     */
+    public Get(address: string, cb: (response: unknown) => unknown, headers: any = null): void {
+        const self = this;
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status === 401) {
+                    self.hooks.Call("http.unauthorized");
+                }
+
+                try {
+                    cb(JSON.parse(xhttp.responseText));
+                } catch (e) {
+                    cb(xhttp.responseText);
+                }
+            }
+        };
+        xhttp.open("GET", address, true);
+
+        if (headers != null) {
+            Object.keys(headers).forEach(k => {
+                const val = headers[k];
+                xhttp.setRequestHeader(k, val);
+            });
+        }
+
+        xhttp.send();
+    }
+
+    /**
+     * make get async request
+     * @param {string} address url to get
+     * @returns {any} json
+     */
+    public async GetAsync(address: string, headers: any = null): Promise<any> {
+        return new Promise((resolve) => {
+            try {
+                this.Get(address, resolve, headers);
+            } catch (exception: unknown) {
+                this.hooks.Call("http.exception", exception);
+                resolve(exception);
+            }
+        });
+    }
+
+    /**
+     * make a post request to a http url and return the result
+     * @param {string} address url
+     * @param {(response: unknown) => void} cb callback executed after request
+     */
+    public Post(address: string, data: any, cb: (response: unknown) => void, headers: any = null) {
+        const self = this;
+        const xhttp = new XMLHttpRequest();
+        const formData = new FormData();
+
+        Object.keys(data).forEach(k => {
+            const value = data[k];
+            formData.append(k, value);
+        });
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status === 401) {
+                    self.hooks.Call("http.unauthorized");
+                }
+                try {
+                    cb(JSON.parse(xhttp.responseText));
+                } catch (e) {
+                    cb(xhttp.responseText);
+                }
+            }
+        };
+        xhttp.open("POST", address, true);
+
+        if (headers != null) {
+            Object.keys(headers).forEach(k => {
+                const val = headers[k];
+                xhttp.setRequestHeader(k, val);
+            });
+        }
+
+        xhttp.send(formData);
+    }
+
+    /**
+     * make a async post to a http url
+     * @param address
+     * @param data
+     * @returns
+     */
+    public async PostAsync(address: string, data: any, headers: any = null): Promise<any> {
+        return new Promise((resolve) => {
+            try {
+                this.Post(address, data, resolve, headers);
+            } catch (exception: unknown) {
+                this.hooks.Call("http.exception", exception);
+                resolve(exception);
+            }
+        });
+    }
+
+    /**
+     * make a delete request to a http url and return the result
+     * @param {string} address url
+     * @param {(response: unknown) => void} cb callback executed after request
+     */
+    public Delete(address: string, data: any, cb: (response: unknown) => void, headers: any = null) {
+        const self = this;
+        const xhttp = new XMLHttpRequest();
+        const formData = new FormData();
+        if (data) {
+            Object.keys(data).forEach(k => {
+                const value = data[k];
+                formData.append(k, value);
+            });
+        }
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status === 401) {
+                    self.hooks.Call("http.unauthorized");
+                }
+                try {
+                    cb(JSON.parse(xhttp.responseText));
+                } catch (e) {
+                    cb(xhttp.responseText);
+                }
+            }
+        };
+        xhttp.open("Delete", address, true);
+
+        if (headers != null) {
+            Object.keys(headers).forEach(k => {
+                const val = headers[k];
+                xhttp.setRequestHeader(k, val);
+            });
+        }
+        xhttp.send(formData);
+    }
+
+    /**
+     * make a async delete to a http url
+     * @param address
+     * @param data
+     * @returns
+     */
+    public async DeleteAsync(address: string, data: any, headers: any = null): Promise<any> {
+        return new Promise((resolve) => {
+            try {
+                this.Delete(address, data, resolve, headers);
+            } catch (exception: unknown) {
+                this.hooks.Call("http.exception", exception);
+                resolve(exception);
+            }
+        });
+    }
+}
--- src/Validation/RequiredValidator.ts
+++ src/Validation/RequiredValidator.ts
@@ -10,4 +10,4 @@ export default class RequiredValidator implements IValidator {
 
         return this._regex.test(val.trim());
     }
-}
+}                                                                                                                                                                                                                                            
